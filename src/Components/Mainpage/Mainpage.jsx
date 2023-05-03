b8b3e6dabcd131445364bd7d3c30d2606902c7e5
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-05-03 15:52:49 +0100

    mainpage updated

---
+++ src/Components/Mainpage/Mainpage.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Mainpage = () => {
+    const [postArray, setPostArray]= useState([]);
+    useEffect(()=>{getPost();},[]);
+
+
+  const getPost = () => {
+    let data=[
+      {
+        "postId":"123456",
+        "userName":"anubhav",
+        "postImageURL":"https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+        "timeStamp":"12345",
+        "likes":"1234"
+      },
+      {
+        "postId":"123456",
+        "userName":"anuragini",
+        "postImageURL":"https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+        "timeStamp":"12345",
+        "likes":"1234"
+      },
+      {
+        "postId":"123456",
+        "userName":"anirudh",
+        "postImageURL":"https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+        "timeStamp":"12345",
+        "likes":"1234"
+      }
+    ];
+    setPostArray(data);
+  };
+
+  return (
+    <div>
+      {postArray.map((item,index) => (
+        <Post id={item.postId} userName={item.userName} postImage={item.postImageURL} likes={item.likes} />
+      ))}
+    </div>
+  );
+}
+
+export default MainPage;
+  
