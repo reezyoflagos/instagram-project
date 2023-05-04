c97489b04624719e3912feb9afafbf239c662625
olofindamilola <kdammy2017@gmail.com>
2023-05-04 17:22:52 +0100

    ft: added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Loginpage from './Components/Loginpage/Loginpage'
 import Suggestions from './Components/Suggestions/Suggestions'
-import NavBar from './Components/NavBar/NavBar'
+
 
 const App = () => {
   return (
     <div>
       <Loginpage/>
       <Suggestions/>
-      <NavBar/>
+      
     </div>
   )
 }
